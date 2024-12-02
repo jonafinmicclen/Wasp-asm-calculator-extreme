5592aa059fb6160184c42bc59ca8fd0319d5b198
Jonathan McClen <jonathangmcclen@gmail.com>
2024-12-02 14:39:48 +0000

    plan for hex to decimal

--- Python overflow adding test/Multiply.py
+++ Python overflow adding test/Multiply.py
@@ -10,22 +10,30 @@ b = 0x1111
 
 r1, r2 = Addition.add(a, b)
 print(f'r1: {hex(r1)}, r2: {hex(r2)}')
-exit()
 
-a0 = a >> 8
-a1 = a - (a0 << 8)
+def multiply(a, b):
 
-b0 = b >> 8
-b1 = b - (b0 << 8)
+    a0 = a >> 8
+    a1 = a - (a0 << 8)
 
-# * 0x10000
-r0 = a0*b0
-# * 0x100
-r1 = a1*b0 + a0*b1
-# * 0x1
-r2 = a1*b1
+    b0 = b >> 8
+    b1 = b - (b0 << 8)
 
-final_value_0, final_value_1, final_value_2, final_value_3 = Addition.combine3(r0, r1, r2)
+    # * 0x10000
+    r0 = a0*b0
+    # * 0x100
+    r1 = a1*b0 + a0*b1
+    # * 0x1
+    r2 = a1*b1
+    
+    print(f'r0: {hex(r0)}, r1: {hex(r1)}, r2: {hex(r2)}')
+    exit()
+
+    final_value_0, final_value_1, final_value_2, final_value_3 = Addition.combine3(r0, r1, r2)
+    
+    return final_value_0, final_value_1, final_value_2, final_value_3
+
+final_value_0, final_value_1, final_value_2, final_value_3 = multiply(a, b)
 
 print(f'{str(hex(final_value_0))[2:]}, {str(hex(final_value_1))[2:]}, {str(hex(final_value_2))[2:]}, {str(hex(final_value_3))[2:]}')
 
