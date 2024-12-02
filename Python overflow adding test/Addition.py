ce81614b0f51a9a4e7e2e9b2ff09601c926f0c04
Jonathan McClen <jonathangmcclen@gmail.com>
2024-12-02 12:38:07 +0000

    first commit

---
+++ Python overflow adding test/Addition.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def left_extract(a):
+    return a >> 8
+
+def right_extract(a):
+    return a - (left_extract(a) << 8)
+
+def add(a, b):
+
+    a0 = left_extract(a)
+    a1 = right_extract(a)
+
+    b0 = left_extract(b)
+    b1 = right_extract (b)
+
+    print(f'number: {hex(a)} \nlex: {hex(a0)}, rex: {hex(a1)}')
+
+    right_result_inc_remainder = a1+b1
+    remainder = left_extract(right_result_inc_remainder)
+    right_result_final = right_extract(right_result_inc_remainder)
+
+
+    print(f'remainder: {hex(remainder)}')
+    left_result_final = remainder + a0+b0
+
+    print(f'result: {str(hex(left_result_final))[2:]}{str(hex(right_result_final))[2:]}')
+
+    print(f'correct result: {str(hex(int(a)+int(b)))[2:]}')
+
+    return left_result_final, right_result_final
+
+def combine3(x10000, x100, x):
+    final_value_0 = right_extract(x)
+    intermediate = left_extract(x)
+    intermediate, final_value_1 = add(x100, intermediate)
+    final_value_3, final_value_2 = add(x10000, intermediate)
+
+    return final_value_3 , final_value_2, final_value_1, final_value_0
---
+++ Python overflow adding test/Multiply.py
@@ -0,0 +1,31 @@
+import numpy as np
+import Addition
+import random
+
+a = np.uint16(random.randint(0, 0xFFFF))
+b = np.uint16(random.randint(0, 0xFFFF))
+
+a = 0xFFFF
+b = 0xFFFF
+
+a0 = a >> 8
+a1 = a - (a0 << 8)
+
+b0 = b >> 8
+b1 = b - (b0 << 8)
+
+# * 0x10000
+r0 = a0*b0
+# * 0x100
+r1 = a1*b0 + a0*b1
+# * 0x1
+r2 = a1*b1
+
+final_value_0, final_value_1, final_value_2, final_value_3 = Addition.combine3(r0, r1, r2)
+
+print(f'{str(hex(final_value_0))[2:]}, {str(hex(final_value_1))[2:]}, {str(hex(final_value_2))[2:]}, {str(hex(final_value_3))[2:]}')
+
+print(f'{hex(int(a)*int(b))}')
+
+# Final step is to implement the adding ffs.... This also means i could have used a for loop of length b of adding a to itself. woudl have been slow like that tho
+
