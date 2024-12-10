92ec564c51429f0c4f36300b9fe70d2ab31ce9d2
Jonathan McClen <jonathangmcclen@gmail.com>
2024-12-10 18:37:10 +0000

    Multiplication fully connected

--- outputtextgenerator.py
+++ outputtextgenerator.py
@@ -1,10 +1,14 @@
 # Generates wasp code for storing text into memory for display
 # Example output text001: DC.W 'a'
 
-num = 0
 
-with open('generatedoutputtext.txt', 'w') as file:
-    text_to_generate = 'Please enter your expression in the format "a+b=" max input number is 65535:\n'
-    for char in text_to_generate:
-        file.write(f'text{num}: DC.W \'{char}\'\n')
-        num+=1
+def generate(text_to_generate, name):
+    num = 0
+    with open('generatedoutputtext.txt', 'w') as file:
+        for char in text_to_generate:
+            file.write(f'{name}{num}: DC.W \'{char}\'\n')
+            num+=1
+            
+# 'Please enter your expression in the format "a+b=" max input number is 65535:\n'
+
+generate('You entered:\n', 'youentered')
